dbdfad06bd1761c298e97c188e6f21d994b1dfe6
Anton Todorov <shadowtabris84@gmail.com>
2023-07-20 14:22:32 +0300

    UC#10 Implement regex tests

--- src/App.js
+++ src/App.js
@@ -18,6 +18,57 @@ function App() {
 
   return (
     <main className="App">
+      <h1>Validator</h1>
+
+      <dl>
+        <dt><code>validate('Test123#', 10)</code> expected True</dt>
+        <dd>Result: {validate('Test123#', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('Test123#Extra', 10)</code> expected False - Exceeds maxLength</dt>
+        <dd>Result: {validate('Test123#Extra', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('test123#', 10)</code> expected False - No uppercase letter</dt>
+        <dd>Result: {validate('test123#', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('TEST123#', 10)</code> expected False - No lowercase letter</dt>
+        <dd>Result: {validate('TEST123#', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('Testtest#', 10)</code> expected False - No digit</dt>
+        <dd>Result: {validate('Testtest#', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('Test123test', 10)</code> expected False - No special character</dt>
+        <dd>Result: {validate('Test123test', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('Test 123#', 10)</code> expected False - Contains whitespace</dt>
+        <dd>Result: {validate('Test 123#', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('', 10)</code> expected False - Empty string</dt>
+        <dd>Result: {validate('', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('T', 10)</code> expected False - Does not meet any condition except having an uppercase letter</dt>
+        <dd>Result: {validate('T', 10).toString()}</dd>
+      </dl>
+
+      <dl>
+        <dt><code>validate('Test123#ExtraLongPassword', 10)</code> expected False - Exceeds maxLength and no special character</dt>
+        <dd>Result: {validate('Test123#ExtraLongPassword', 10).toString()}</dd>
+      </dl>
     </main>
   );
 }
